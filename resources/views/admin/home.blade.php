592ba2819ace86edd80dc192bcb393e4dff107bd
quanghung97 <hungcan1997@gmail.com>
2017-11-10 16:44:19 +0700

    four fix frontend

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -101,12 +101,7 @@
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
-                        <li>
-                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-                        </li>
-                        <li>
-                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                        </li>
+                        
                        
                        
                      
