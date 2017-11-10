d1c02e8d64e50440f0db41d5498c7a80540e93bc
quanghung97 <hungcan1997@gmail.com>
2017-11-10 13:18:16 +0700

    second fix frontend

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.admin_dashboard')
+@section('sidebar')
+    <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <button href="abc.html" type="button" class="btn btn-danger">Create request</button>
+                               
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                    
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                               <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-flag" href="flot.html">    FeedBack</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-log-out" href="flot.html">    Closed</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        </li>
+                       
+                       
+                     
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+@endsection        
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -267,32 +267,98 @@
                     <ul class="nav" id="side-menu">
                         <li class="sidebar-search">
                             <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
+                                <button href="abc.html" type="button" class="btn btn-danger">Create request</button>
+                               
                             </div>
                             <!-- /input-group -->
                         </li>
                         <li>
                             <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
+                    
                         <li>
                             <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="flot.html">Flot Charts</a>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Morris.js Charts</a>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
                                 </li>
                                 <li>
-                                    <a href="flot.html">Flot Charts</a>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
                                 </li>
                                 <li>
-                                    <a href="flot.html">Flot Charts</a>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                               <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-check" href="flot.html">    Resolved</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a class="glyphicon glyphicon-tasks" href="flot.html">    All</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-envelope" href="morris.html">    New</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-forward" href="flot.html">    Progess</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-flag" href="flot.html">    FeedBack</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-calendar" href="flot.html">    Out Of Date</a>
+                                </li>
+                                <li>
+                                    <a class="glyphicon glyphicon-log-out" href="flot.html">    Closed</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
@@ -303,92 +369,31 @@
                         <li>
                             <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
                         </li>
-                        <li>
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="panels-wells.html">Panels and Wells</a>
-                                </li>
-                                <li>
-                                    <a href="buttons.html">Buttons</a>
-                                </li>
-                                <li>
-                                    <a href="notifications.html">Notifications</a>
-                                </li>
-                                <li>
-                                    <a href="typography.html">Typography</a>
-                                </li>
-                                <li>
-                                    <a href="icons.html"> Icons</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grid</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                    <!-- /.nav-third-level -->
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="blank.html">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="login.html">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
+                       
+                       
+                     
                     </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
             </div>
             <!-- /.navbar-static-side -->
         </nav>
-
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Dashboard</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-     
-            <!-- /.row -->
-         
-            <!-- /.row -->
+        
+        
+        
+<!--main noi dung-->
+    <div id="page-wrapper" class="page-content" style="background: white;">
+        <!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+        <div class="content sm-gutter">
+            <ul class="breadcrumb">
+                <li>
+                    <p>TRANG CHỦ</p>
+                </li>
+                <li><a href="#" class="active">@yield('name_page')</a></li>
+            </ul>
+            @yield('main')
         </div>
+    </div>
         <!-- /#page-wrapper -->
 
     </div>
