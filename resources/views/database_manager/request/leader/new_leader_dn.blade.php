9c16a023acc7d8dbf13eb37a703bfe6f2fade24a
nguyenthanhtung200997 <lonelystarladykillah@gmail.com>
2017-11-14 17:43:53 +0700

    front-end A1 v1.1

--- resources/views/database_manager/request/leader/new_leader_dn.blade.php
+++ resources/views/database_manager/request/leader/new_leader_dn.blade.php
@@ -1,13 +1,7 @@
-@extends('admin.home_leader_dn')
-@section('css')
+@extends('admin.home_leader_dn') @section('css')
 <!--link css only page here-->
 
-@endsection
-
-
-
-
-@section('main')
+@endsection @section('main')
 <!--thiet ke form html nhap cong viec (request)-->
 <div class="row">
 	<h3>Thêm yêu cầu</h3>
@@ -21,9 +15,11 @@
 
 		<div class="form-group col-md-6">
 			<label for="Mucdouutien">Mức độ ưu tiên</label>
-			<select name="Mucdouutien" id="mucdouutien">
+			<br>
+			<select name="Mucdouutien" id="mucdouutien" class="form-control">
 				<option value="">Thấp</option>
 				<option value="" selected>Bình thường</option>
+				<option value="">Cao</option>
 				<option value="">Cao</option>
 				<option value="">Khẩn cấp</option>
 			</select>
@@ -31,12 +27,21 @@
 
 		<div class="form-group col-md-6">
 			<label for="ngayhethan">Ngày hết hạn</label>
-			<input type="text" id="ngayhethan" name="ngayhethan">
+			<div class="form-group">
+				<div class="input-group date" id="datetimepicker1">
+					<input type="text" class="form-control" />
+					<span class="input-group-addon">
+						<span class="glyphicon glyphicon-calendar"></span>
+					</span>
+				</div>
+			</div>
+
 		</div>
 
 		<div class="form-group col-md-6">
 			<label for="bophanit">Bộ phận IT</label>
-			<select name="bophanit" id="bophanit">
+			<br>
+			<select name="bophanit" id="bophanit" class="form-control">
 				<option value="">Hanoi-IT</option>
 				<option value="">Đà Nẵng-IT</option>
 			</select>
@@ -44,31 +49,37 @@
 
 		<div class="form-group col-md-6">
 			<label for="nguoilienquan">Người liên quan</label>
-			<input type="text" name="nguoilienquan" id="nguoilienquan" />
+			<br>
+			<input type="text" name="nguoilienquan" id="nguoilienquan" class="form-control" />
 		</div>
 
 		<div class="form-group col-md-12">
 			<label for="noidung">Nội dung</label>
 			<br/>
-			<textarea name="noidung" id="noidung" cols="100" rows="10"></textarea>
+			<textarea name="noidung" id="noidung" class="form-control"></textarea>
 		</div>
 
 	</form>
 
-	<div class="col-md-12">
-		<input type="text" class="uploadfile" id="uploadfile" placeholder="choose file to upload">
+	<form class="form-horizontal">
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="uploadfile" placeholder="choose file to upload">
+		</div>
 		<button class="btn btn-primary">Choose File</button>
-		<br/>
-
+	</form>
+	<hr>
+	<form>
 		<input type="submit" class="btn btn-info" value="Gửi yêu cầu">
 		<button type="button">Hủy bỏ</button>
-	</div>
+	</form>
 </div>
 
+@endsection @section('js')
+<script type="text/javascript">
+	$(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
 
-@endsection
-
-
-@section('js')
+</script>
 <script src="{{ URL::asset('public/dist/js/sb-admin-2.js') }}"></script>
 @endsection
